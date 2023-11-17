24cc8c8c014dcb02cf188f4557713818394234d9
CodeSandbox <support@codesandbox.io>
2023-11-17 16:20:41 +0000

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-var http = require("http");
+const express = require('express');
+const expressApp = express();
+const bodyParser = require('body-parser');
+const fs = require('fs');
 
-//create a server object:
-http
-  .createServer(function (req, res) {
-    res.write("Hello from CodeSandbox!"); //write a response to the client
-    res.end(); //end the response
-  })
-  .listen(8080); //the server object listens on port 8080
+expressApp.set('view engine', 'pug');
+expressApp.set('views', 'views');
+
+expressApp.use(bodyParser.urlencoded({ extended: false }));
+
+expressApp.get('/', (req, res) => {
+res.render('form');
+});
+
+expressApp.post('/', (req, res) => {
+  var formText = req.body.text;
+
+  fs.writeFile("form.txt", formText, (error) => {
+    if(error) {
+      console.log(error);
+    } else {
+      console.log('file created')
+    }
+  });
+
+  res.redirect("/");
+});
+
+expressApp.listen(3000, () => {
+  console.log('listening in port 3000')
+});
